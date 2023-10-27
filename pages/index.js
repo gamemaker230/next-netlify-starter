1c16b2a0157e5142f589b5eec52854ad1caea0eb
gamemaker230 <149206812+gamemaker230@users.noreply.github.com>
2023-10-27 17:55:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,51 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
+
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+<body>
+    <ul>
+  <li><a>Angel's Unblocked Games</a></li>
+ 
+</ul>
+</body>
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Angel's Unblocked Games
-        </p>
       </main>
 
       <Footer />
